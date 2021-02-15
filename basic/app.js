b1cc66ee9ba9634f358058c2edd7464e5393f9fc
시혁 박 <exon.indiv@gmail.com>
2021-02-15 21:50:30 +0900

    web

--- basic/app.js
+++ basic/app.js
@@ -1,16 +1,22 @@
 const changeTheme = document.getElementById('changetheme')
-const logo = document.getElementById('logo')
 
 changeTheme.addEventListener('click', () => {
+    const logo = document.getElementById('logo')
+    const theme = document.getElementById('changetheme')
+
     const html = document.documentElement
 
     if (html.classList.contains('dark')) {
         html.classList.remove('dark')
         logo.src = './images/logo2.png'
+        theme.style.background = 'url(\"./images/dark.png\")'
+        theme.style.backgroundSize = '30px 30px'
         localStorage.setItem('darkTheme', 'false')
     } else {
         html.classList.add('dark')
         logo.src = './images/logo1.png'
+        theme.style.background = 'url(\"./images/light.png\")'
+        theme.style.backgroundSize = '30px 30px'
         localStorage.setItem('darkTheme', 'true')
     }
 })
